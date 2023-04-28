fbb611fc5638d42a715949551b264c693d22425e
Kaushal Regmi <regmikaushal321@gmail.com>
2023-04-28 20:06:07 +0545

    utils added plus logic change

--- src/app/components/form/index.tsx
+++ src/app/components/form/index.tsx
@@ -1,40 +1,47 @@
 "use client";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import InputField from "../inputs";
-import { IConvertFieldState } from "@/app/types/ConverterFieldTypes";
+import { checkTwoDecimalNumber } from "@/app/utils/regex.utils";
+import {
+  convertionRateToAnother,
+  setChangeHandler,
+} from "@/app/utils/convertion.utils";
+import {ArrowPathRoundedSquareSolid} from '@graywolfai/react-heroicons';
+import { FORM_CONSTANTS } from "@/app/utils/constants";
 const Form = () => {
-  const [convertField, setConvertionField] = useState<IConvertFieldState>({
-    nepali: "",
-    busd: "",
-  });
-  const onChangeHandler = (e: any) => {
-    const [name, value] = e.target;
-    setConvertionField({
-      ...convertField,
-      [name]: value,
-    });
+  const [fromCurrency, setFromCurrency] = useState("");
+  const [toCurrency, setToCurrency] = useState("");
+
+  const onChangeHandler = async (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (value !== "" && !checkTwoDecimalNumber(value)) return;
+    setChangeHandler(name, value, setFromCurrency, setToCurrency);
+    convertionRateToAnother(name, value, setFromCurrency, setToCurrency);
   };
+
   return (
     <form>
       <div className="grid gap-4 mb-6 md:grid-cols-1">
+        <h1 className="font-bold text-xl">{FORM_CONSTANTS.TITLE}</h1>
         <InputField
           inputClass="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2 px-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          inputName="nepali"
+          inputName="fromCurrency"
           placeHolder="0.00"
           isRequired
+          value={fromCurrency}
           labelClass="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           labelName="NEP"
-          type="text"
           onChangeHandler={onChangeHandler}
         />
+        <ArrowPathRoundedSquareSolid/>
         <InputField
           inputClass="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2 px-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          inputName="busd"
+          inputName="toCurrency"
           placeHolder="0.00"
           isRequired
           labelClass="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           labelName="BUSD"
-          type="text"
+          value={toCurrency}
           onChangeHandler={onChangeHandler}
         />
       </div>
--- src/app/components/inputs/index.tsx
+++ src/app/components/inputs/index.tsx
@@ -7,6 +7,7 @@ const InputField = ({
   placeHolder,
   isRequired,
   type,
+  value
 }: {
   inputName: string;
   onChangeHandler: Function;
@@ -15,16 +16,16 @@ const InputField = ({
   labelClass: string;
   placeHolder: string;
   isRequired: boolean;
-  type: string;
+  type?: string;
+  value?:string;
 }) => (
   <>
     <div>
-      {/* <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required> */}
-      <label className={labelClass}>{labelName}</label>
+     <label className={labelClass}>{labelName}</label>
       <input
         type={type || "text"}
         name={inputName}
+        value={value}
         className={inputClass}
         placeholder={placeHolder || ""}
         required={isRequired}
