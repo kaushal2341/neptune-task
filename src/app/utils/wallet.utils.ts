8799f6adcf1f1e8274c5a1d3508b0b566dd500dd
Kaushal Regmi <regmikaushal321@gmail.com>
2023-05-10 00:29:31 +0545

    Simple conditional logic changed

--- src/app/utils/wallet.utils.ts
+++ src/app/utils/wallet.utils.ts
@@ -1,30 +1,26 @@
 import { getBalance, getChainId } from "../services/ether";
 
-export const connectToWallet = async() =>{
-
- if(typeof window?.ethereum==='undefined'){
-   console.log('not detected');
- }else{
-    console.log("detected",window?.ethereum);
+export const connectToWallet = async () => {
+  if (!window.ethereum) {
+    console.log("not detected");
+  } else {
+    console.log("detected", window?.ethereum);
     const accounts = await getAccounts();
-    if(accounts.length){
-     const balance = await getBalance(accounts[0]);
-     const chainId = await getChainId();
+    if (accounts.length) {
+      const balance = await getBalance(accounts[0]);
+      const chainId = await getChainId();
     }
+  }
+};
 
- }
- 
-}
-
-const getAccounts = async() =>{
-
-    try{
-      const accounts = await   window.ethereum?.request({
-        method:"eth_requestAccounts"
-      })
-      return accounts;
-    }catch(e){
-        console.log("====error",e)
-        return []
-    }
-}
+const getAccounts = async () => {
+  try {
+    const accounts = await window.ethereum?.request({
+      method: "eth_requestAccounts",
+    });
+    return accounts;
+  } catch (e) {
+    console.log("====error", e);
+    return [];
+  }
+};
